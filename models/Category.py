156f8ffff00e8b3ac6cd5150285239400184f8a3
mythao <dvthaomy44@gmail.com>
2024-11-21 12:18:18 +0700

    modify: modify the category to match the ERD

--- models/Category.py
+++ models/Category.py
@@ -5,10 +5,5 @@ class Category(Base):
     __tablename__ = "category"
     category_id = Column(Integer, index=True, primary_key=True)
     name = Column(String, index=True)
-    description = Column(String, index=True)
-    price = Column(BigInteger, index=True)
-    image = Column(String, index=True)
-    color = Column(String, index=True)
-    
     
 print("Category model created successfully.")
--- models/Product.py
+++ models/Product.py
@@ -1,11 +1,11 @@
 
-from sqlalchemy import Column, Integer, String, BigInteger
+from sqlalchemy import Column, Integer, String, BigInteger,ForeignKey
 from database.__init__ import Base 
 class Product(Base):
     __tablename__ = "product"
     
     product_id = Column(Integer,primary_key=True, index=True)
-    category_id = Column(Integer, index=True)
+    category_id = Column(Integer, ForeignKey('category.category_id') ,index=True)
     name = Column(String, index=True)
     description = Column(String, index=True)
     price = Column(BigInteger, index=True)

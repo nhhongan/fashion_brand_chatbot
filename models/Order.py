6925ca8c40732e7e58235fcaf2a83057632727aa
QUAN <miqua2308@gmail.com>
2024-11-23 15:31:14 +0700

    modify: Change the erd for opt

--- models/Order.py
+++ models/Order.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, BigInteger
+from sqlalchemy import Column, Integer, String, BigInteger, ForeignKey
 from database.__init__ import Base 
 
 class Order(Base):
@@ -7,8 +7,7 @@ class Order(Base):
     order_id = Column(Integer, primary_key=True, index=True,autoincrement=True)
     user_id = Column(Integer, index=True)
     total_price = Column(BigInteger, index=True)
-    payment_id = Column(Integer, index=True)
-    
+    payment_id = Column(Integer, ForeignKey('payment.payment_id'), index=True)
     
     
 print("Order model created successfully.")
